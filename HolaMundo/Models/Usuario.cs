614954b11c76ba77a16a46425466d3c56ed34b87
toxidomass@hotmail.com <toxidomass@hotmail.com>
2020-08-25 11:04:50 -0500

    Primer Comit

---
+++ HolaMundo/Models/Usuario.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace HolaMundo.Models
+{
+     public class Usuario
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Correo { get; set; }
+        public int Edad { get; set; }
+
+    }
+}
